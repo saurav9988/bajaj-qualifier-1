02e2b95b7445401aaefd5b0efd4ecb8aa57b9503
saurav9988 <98246021+saurav9988@users.noreply.github.com>
2024-09-22 16:14:19 +0530

    Add files via upload
    updated

---
+++ Bajaj-main/backend/src/routes/userRoutes.js
@@ -0,0 +1,45 @@
+const express = require('express');
+
+const router = new express.Router();
+
+router.get('/bfhl', (req, res) => {
+    try{
+        res.status(200).json({ operation_code: 1 });
+    }catch(e){
+        res.status(500).json({
+            message : "Internal Server Error!"
+        });
+    }
+});
+  
+
+router.post('/bfhl', (req, res) => {
+    const { data } = req.body;
+    const user_id = "Saurav_Kumar_04072002";
+    const email = "sv9510@srmist.edu.in";
+    const roll_number = "RA2111026010061";
+  
+    if (!data || !Array.isArray(data)) {
+      return res.status(400).json({ is_success: false });
+    }
+  
+    const numbers = data.filter(item => !isNaN(item));
+    const alphabets = data.filter(item => /^[a-zA-Z]$/.test(item));
+    const highest_alphabet = alphabets.length > 0 ? [alphabets.sort().pop()] : [];
+
+    if (highest_alphabet.length > 0) {
+        alphabets.push(highest_alphabet[0]);
+      }
+    
+  
+    res.json({
+      is_success: true,
+      user_id,
+      email,
+      roll_number,
+      numbers,
+      alphabets,
+      highest_alphabet
+    });
+  });
+module.exports = router;
